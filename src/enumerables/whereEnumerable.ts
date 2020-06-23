6b303bad1691b9d61ab891006dabc3b6723b0e21
Martin Volek <martin@vdolek.cz>
2020-06-23 09:13:09 +0200

    files

---
+++ src/enumerables/whereEnumerable.ts
@@ -0,0 +1,18 @@
+import { Enumerable } from '@src/enumerables/enumerable';
+import { EnumerableBase } from '@src/enumerables/enumerableBase';
+
+export class WhereEnumerable<T> extends EnumerableBase<T> {
+    constructor(
+        private readonly innerEnumerable: Enumerable<T>,
+        private readonly predicate: (x: T) => boolean) {
+        super();
+    }
+
+    * [Symbol.iterator](): Iterator<T> {
+        for (const item of this.innerEnumerable) {
+            if (this.predicate(item)) {
+                yield item;
+            }
+        }
+    }
+}
