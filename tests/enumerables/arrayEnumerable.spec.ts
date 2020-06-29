20d33b1699045c38dc86015ce8b95adfabb05628
Martin Volek <Martin.Volek@ext.csas.cz>
2020-06-29 13:51:41 +0200

    separated tests

--- tests/enumerables/arrayEnumerable.spec.ts
+++ tests/enumerables/arrayEnumerable.spec.ts
@@ -5,6 +5,7 @@ describe('Options tests', () => {
     it('checking default options', () => {
         const enumerable = new ArrayEnumerable([2, 4, 6]);
 
+        enumerable.where(x => x > 3);
         expect(enumerable.toArray()).to.be.deep.equal([2, 4, 6]);
     });
 });
