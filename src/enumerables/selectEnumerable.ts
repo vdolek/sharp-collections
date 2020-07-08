697172b3623a991d5f4e02154fa6ee87c6a2ac8d
Martin Volek <martin@vdolek.cz>
2020-07-08 11:51:05 +0200

    rename

--- src/enumerables/selectEnumerable.ts
+++ src/enumerables/selectEnumerable.ts
@@ -2,14 +2,14 @@ import { Enumerable } from '@src/internal';
 
 export class SelectEnumerable<T, TResult> extends Enumerable<TResult> {
     public constructor(
-        private readonly innerEnumerable: Enumerable<T>,
+        private readonly source: Enumerable<T>,
         private readonly selector: ((x: T) => TResult) | ((x: T, idx: number) => TResult)) {
         super();
     }
 
     public *[Symbol.iterator](): Iterator<TResult> {
         let index = 0;
-        for (const element of this.innerEnumerable) {
+        for (const element of this.source) {
             yield this.selector(element, index);
             ++index;
         }
--- src/enumerables/selectManyEnumerable.ts
+++ src/enumerables/selectManyEnumerable.ts
@@ -2,13 +2,13 @@ import { Enumerable } from '@src/internal';
 
 export class SelectManyEnumerable<T, TResult> extends Enumerable<TResult> {
     public constructor(
-        private readonly innerEnumerable: Enumerable<T>,
+        private readonly source: Enumerable<T>,
         private readonly selector: (x: T) => Enumerable<TResult>) {
         super();
     }
 
     public *[Symbol.iterator](): Iterator<TResult> {
-        for (const element of this.innerEnumerable) {
+        for (const element of this.source) {
             const subEnumerable = this.selector(element);
             for (const x of subEnumerable) {
                 yield x;
