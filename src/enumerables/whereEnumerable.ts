7518b3a5d33d5e9de8d97a2d845ab415c322e708
Martin Volek <Martin.Volek@ext.csas.cz>
2020-07-08 11:51:05 +0200

    rename

--- src/enumerables/whereEnumerable.ts
+++ src/enumerables/whereEnumerable.ts
@@ -2,14 +2,14 @@ import { Enumerable } from '@src/internal';
 
 export class WhereEnumerable<T> extends Enumerable<T> {
     public constructor(
-        private readonly innerEnumerable: Enumerable<T>,
+        private readonly source: Enumerable<T>,
         private readonly predicate: ((item: T) => boolean) | ((item: T, index: number) => boolean)) {
         super();
     }
 
     public *[Symbol.iterator](): Iterator<T> {
         let index = 0;
-        for (const element of this.innerEnumerable) {
+        for (const element of this.source) {
             if (this.predicate(element, index)) {
                 yield element;
             }
