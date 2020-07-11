2c55b9c543692784787d974eca23a8d4528fcffa
Martin Volek <me@martinvolek.cz>
2020-07-11 22:36:57 +0200

    groupBy with elementSelector

--- src/enumerables/groupByEnumerable.ts
+++ src/enumerables/groupByEnumerable.ts
@@ -1,15 +1,16 @@
 import { Dictionary, Enumerable, Grouping, List } from '@src/internal';
 
-export class GroupByEnumerable<TKey, TValue, TResult = Grouping<TKey, TValue>> extends Enumerable<TResult> {
+export class GroupByEnumerable<TKey, TValue, TElement = TValue, TResult = Grouping<TKey, TElement>> extends Enumerable<TResult> {
     public constructor(
         protected readonly source: Iterable<TValue>,
         protected readonly keySelector: (value: TValue, index: number) => TKey,
-        protected readonly resultSelector?: (key: TKey, group: Enumerable<TValue>) => TResult) {
+        protected readonly elementSelector?: (value: TValue, index: number) => TElement,
+        protected readonly resultSelector?: (key: TKey, group: Enumerable<TElement>) => TResult) {
         super();
     }
 
     public [Symbol.iterator](): Iterator<TResult> {
-        const dict = new Dictionary<TKey, List<TValue>>();
+        const dict = new Dictionary<TKey, List<TElement>>();
 
         let index = 0;
         for (const element of this.source) {
@@ -19,11 +20,11 @@ export class GroupByEnumerable<TKey, TValue, TResult = Grouping<TKey, TValue>> e
                 dict.add(key, new Grouping(key));
             }
 
-            dict.get(key).add(element);
+            const mappedElement = this.elementSelector != null ? this.elementSelector(element, index) : element as unknown as TElement;
+            dict.get(key).add(mappedElement);
         }
 
-        // @ts-ignore
-        const resSelector = this.resultSelector ?? ((key, group) => new Grouping(key, group) as TResult);
+        const resSelector = this.resultSelector ?? ((key, group) => new Grouping(key, group) as unknown as TResult);
         const result = dict.select(pair => resSelector(pair.key, pair.value));
         return result[Symbol.iterator]();
     }
--- tests/enumerables/groupBy.spec.ts
+++ tests/enumerables/groupBy.spec.ts
@@ -23,16 +23,48 @@ describe('groupBy tests', () => {
         ]);
     });
 
+    it('ElementSelector test', () => {
+        const list = List.from('Hello', 'Hi', 'Good morning', 'Bye', 'Good evening');
+        const grouped = list
+            .groupBy(x => x[0], x => x.toUpperCase())
+            .toList();
+
+        const asArray = grouped
+            .select(x => x.toArray())
+            .toArray();
+
+        expect(asArray).to.be.deep.equal([
+            ['HELLO', 'HI'],
+            ['GOOD MORNING', 'GOOD EVENING'],
+            ['BYE']
+        ]);
+    });
+
     it('ResultSelector test', () => {
         const list = List.from('Hello', 'Hi', 'Good morning', 'Bye', 'Good evening');
         const groupedAsArray = list
-            .groupBy(x => x[0], (key, group) => group.toArray())
+            .groupBy(x => x[0], undefined, (key, group) => group.toArray())
             .toArray();
 
         expect(groupedAsArray).to.be.deep.equal([
             ['Hello', 'Hi'],
             ['Good morning', 'Good evening'],
             ['Bye']
+        ]);
+    });
+
+
+
+    it('ResultSelector test', () => {
+        const list = List.from('Hello', 'Hi', 'Good morning', 'Bye', 'Good evening');
+        const groupedAsArray = list
+            .groupBy(x => x[0], x => x.toUpperCase(), (key, group) => group.toArray())
+            .toArray();
+
+        expect(groupedAsArray).to.be.deep.equal([
+            ['HELLO', 'HI'],
+            ['GOOD MORNING', 'GOOD EVENING'],
+            ['BYE']
         ]);
     });
 });
