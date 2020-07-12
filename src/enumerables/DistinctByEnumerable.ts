7871a8965110449df89a77bd62ff48a7de9d399a
Martin Volek <me@martinvolek.cz>
2020-07-12 12:52:18 +0200

    distinct

---
+++ src/enumerables/DistinctByEnumerable.ts
@@ -0,0 +1,23 @@
+import { Enumerable, HashSet } from '@src/Internal';
+
+export class DistinctByEnumerable<T, TKey = T> extends Enumerable<T> {
+    public constructor(
+        private readonly source: Iterable<T>,
+        private readonly keySelector?: (element: T, index: number) => TKey
+    ) {
+        super();
+    }
+
+    public *[Symbol.iterator](): Iterator<T> {
+        const set = new HashSet<TKey>();
+
+        let index = 0;
+        for (const element of this.source) {
+            const key = this.keySelector != null ? this.keySelector(element, index++) : element as unknown as TKey;
+            if (!set.contains(key)) {
+                set.add(key);
+                yield element;
+            }
+        }
+    }
+}
--- tests/enumerables/ArrayEnumerable.spec.ts
+++ tests/enumerables/ArrayEnumerable.spec.ts
@@ -2,7 +2,7 @@ import { ArrayEnumerable } from '@src/Internal';
 import { expect } from 'chai';
 
 describe('ArrayEnumerable tests', () => {
-    it('toArray', () => {
+    it('toArray test', () => {
         const arrayEnumerable = new ArrayEnumerable([2, 4, 6]);
 
         expect(arrayEnumerable.toArray()).to.be.deep.equal([2, 4, 6]);
