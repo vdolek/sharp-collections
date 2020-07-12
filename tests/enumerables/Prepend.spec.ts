7f93a1378e2f6de7906adc58134ee187582fdb61
Martin Volek <martin@vdolek.cz>
2020-07-12 16:45:30 +0200

    prepend

---
+++ tests/enumerables/Prepend.spec.ts
@@ -0,0 +1,11 @@
+import { Enumerable } from '@sharp-collections';
+import { expect } from 'chai';
+
+describe('prepend tests', () => {
+    it('simple test', () => {
+        const list = Enumerable.fromElements(2, 4, 6);
+        const appended = list.prepend(99).toArray();
+
+        expect(appended).to.be.deep.equal([99, 2, 4, 6]);
+    });
+});
