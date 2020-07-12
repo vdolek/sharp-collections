5d2526467238c4b326bb84c2c8e280b12f1c939f
Martin Volek <martin@vdolek.cz>
2020-07-12 13:51:36 +0200

    union

---
+++ tests/enumerables/Union.spec.ts
@@ -0,0 +1,12 @@
+import { List } from '@src/Internal';
+import { expect } from 'chai';
+
+describe('union tests', () => {
+    it('simple test', () => {
+        const list1 = List.fromElements(1, 2, 3, 2);
+        const list2 = List.fromElements(1, 3, 4);
+        const list3 = list1.union(list2);
+
+        expect(list3.toArray()).to.be.deep.equal([1, 2, 3, 4]);
+    });
+});
