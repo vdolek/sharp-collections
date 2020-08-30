2e98bbb57ff8f15fd17836617b1763fef19108af
Martin Volek <martin@vdolek.cz>
2020-08-30 18:49:36 +0200

    list remove

--- src/collections/List.ts
+++ src/collections/List.ts
@@ -40,6 +40,10 @@ export class List<T> extends ReadOnlyList<T> {
         return this;
     }
 
+    public remove(index: number): void {
+        this.source.splice(index, 1);
+    }
+
     public set(index: number, value: T): void {
         if (index < 0 || index >= this.source.length) {
             throw new Error('The index was out of range of the list.');
