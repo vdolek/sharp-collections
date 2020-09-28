7fe314d03f75ddf232b3abb22ed85590f08a740a
Martin Volek <me@martinvolek.cz>
2020-09-28 19:09:17 +0200

    HashCode fix

--- src/hashing/HashCode.ts
+++ src/hashing/HashCode.ts
@@ -1,43 +1,54 @@
 /* tslint:disable:no-bitwise */
 
+import { Enumerable } from '../collections/Enumerable';
+
 export class HashCode {
     private static lastHashCode = 0;
     private static readonly objectHashCodes = new WeakMap<{ }, number>();
 
     public static getHashCode<T>(value: T): number {
         if (value == null) {
-            return 0;
+            return 561691612;
         }
 
-        if (typeof value === 'number') {
-            if (isFinite(value)) {
-                return HashCode.getNumberHashCode(value);
-            }
+        switch (typeof value) {
+            case 'boolean': return HashCode.hashBoolean(value);
+            case 'number': return HashCode.hashNumber(value);
+            case 'string': return HashCode.hashString(value);
+            case 'object': return HashCode.hashObject(value);
+            default: throw new Error(`Not supported type for hashing (${typeof value})`);
+        }
+    }
 
-            return 1;
+    public static combine(hashCodes: Iterable<number>): number {
+        return HashCode.combineInner(hashCodes);
+    }
+
+    public static combineParams(...hashCodes: number[]): number {
+        return HashCode.combineInner(hashCodes);
+    }
+
+    private static hashBoolean(bool: boolean): number {
+        return HashCode.hashNumber(bool ? 1 : 0);
+    }
+
+    private static hashNumber(num: number): number {
+        if (Number.isFinite(num)) {
+            return HashCode.combineInner(Enumerable.single(num), 2354214525);
         }
 
-        if (typeof value === 'string') {
-            return HashCode.getStringHashCode(value);
-        }
-
-        return HashCode.getObjectHashCode(value);
+        return 0;
     }
 
-    private static getNumberHashCode(value: number): number {
-        const hashCode = value | 0; // convert to 32bit integer
-        return hashCode;
+    private static hashString(value: string): number {
+        const charCodes = Enumerable.from(value).select(x => x.charCodeAt(0));
+        return HashCode.combineInner(charCodes, 1745632457); // TODO test
     }
 
-    private static getStringHashCode(value: string): number {
-        const hashCode = HashCode.cyrb53(value);
-        return hashCode;
-    }
-
-    private static getObjectHashCode<T>(value: T): number {
+    private static hashObject<T>(value: T): number {
         let hashCode = HashCode.objectHashCodes.get(value);
         if (hashCode == null) {
-            hashCode = HashCode.getNumberHashCode(++HashCode.lastHashCode);
+            hashCode = HashCode.hashNumber(++HashCode.lastHashCode);
             HashCode.objectHashCodes.set(value, hashCode);
         }
 
@@ -45,14 +56,13 @@ export class HashCode {
     }
 
     // source: https://stackoverflow.com/a/52171480/2262449
-    private static cyrb53(str: string, seed: number = 0): number {
+    private static combineInner(source: Iterable<number>, seed: number = 0): number {
         let h1 = seed ^ 0xDEADBEEF;
         let h2 = seed ^ 0x41C6CE57;
 
-        for (let i = 0, ch; i < str.length; i++) {
-            ch = str.charCodeAt(i);
-            h1 = Math.imul(h1 ^ ch, 2654435761);
-            h2 = Math.imul(h2 ^ ch, 1597334677);
+        for (const hashCode of source) {
+            h1 = Math.imul(h1 ^ hashCode, 2654435761);
+            h2 = Math.imul(h2 ^ hashCode, 1597334677);
         }
 
         h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
---
+++ tests/hashing/HashCode.spec.ts
@@ -0,0 +1,23 @@
+// import { HashCode } from '../../src/hashing/HashCode';
+//
+// describe.only('HashCode tests', () => {
+//     it('simple test', () => {
+//         test(false);
+//         test(true);
+//
+//         test(0);
+//         test(1);
+//
+//         test('0');
+//         test('1');
+//
+//         test('hello');
+//
+//         test(new Date());
+//         test(new Date());
+//     });
+//
+//     function test<T>(value: T): void {
+//         console.log(`${value}: ${HashCode.getHashCode(value)}`);
+//     }
+// });
